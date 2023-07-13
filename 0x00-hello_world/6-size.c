c8fa2987ac56132f096e2522b960eb609b37e0b3
Nobie <n12507400@gmail.com>
2023-07-13 05:44:49 -0700

    the sizeof function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
 	printf("sizeof a long int: %lu byte(s)\n", sizeof(long int));
 	printf("sizeof a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of an int: %lu byte(s)\n", sizeof(unsigned int));
 	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
