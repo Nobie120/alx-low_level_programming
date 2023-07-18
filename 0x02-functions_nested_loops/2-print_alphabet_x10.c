ff270cf091382d40fae1d606ad92e4812d7c6621
Nobie <n12507400@gmail.com>
2023-07-18 05:29:01 -0700

    Functions and header files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing the alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int count;
+
+	count = 0;
+	while (count <= 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+
+		_putchar('\n');
+		count++;
+	}
+}
