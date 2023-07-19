5ee357302dc7c718700108be62f5902dc6255544
Nobie <n12507400@gmail.com>
2023-07-19 04:16:17 -0700

    Testing a function

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * positive_or_negative - determines if num is +, - or 0
+ * @i:The num tha is checked
+ */
+
+void positive_or_negative(int i)
+{
+	if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is postive\n", i);
+}
