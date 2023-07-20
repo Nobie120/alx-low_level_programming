c4b920c4fc593bc3f47ce4ce00bba5eda3dc0127
Nobie <n12507400@gmail.com>
2023-07-20 15:48:32 -0700

    Function

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print 1-100 but fix if multiple of 3 and buzzfor 5
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
