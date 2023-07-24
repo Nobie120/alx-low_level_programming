4ff819747c4abb6ba1eceb60656ef8f6b21b6b01
Nobie <n12507400@gmail.com>
2023-07-24 06:42:53 -0700

    Pointer and array of strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev -  function that prints a string, in revers
+ * @s:string to be printed in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
