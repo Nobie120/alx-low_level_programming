91ae77b9cf6aab3af2af2b936de5dbbfa406a7f8
Nobie <n12507400@gmail.com>
2023-07-26 05:10:17 -0700

    Capitalizing words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - a function that capitalizes all words
+ * @n:The string whose words are capitailized
+ * Return:the capitalized words in a string
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		while (!(n[i] >= 'a' && n[i] <= 'z'))
+			i++;
+		if (n[i - 1] == ' '||
+			n[i - 1] == '\t' ||
+			n[i - 1] == '\n' ||
+			n[i - 1] == ',' ||
+			n[i - 1] == ';' ||
+			n[i - 1] == '.' ||
+			n[i - 1] == '!' ||
+			n[i - 1] == '?' ||
+			n[i - 1] == '"' ||
+			n[i - 1] == '(' ||
+			n[i - 1] == ')' ||
+			n[i - 1] == '{' ||
+			n[i - 1] == '}')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
