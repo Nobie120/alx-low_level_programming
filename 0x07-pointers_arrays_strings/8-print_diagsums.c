464ae3f2c72e642d08ed7ff06fe58ecd0f4c72c0
Nobie <n12507400@gmail.com>
2023-07-31 17:21:43 -0700

    3d array

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * print_diagsums - function that prints the sum of  
+ * print_diagsums - function that prints the sum of
  * two diagonals of a square matrix of integers.
  * the two diagonals of a square matrix of integers
  * @a:array
- * size:size
+ * @size:size
  */
 
 void print_diagsums(int *a, int size)
