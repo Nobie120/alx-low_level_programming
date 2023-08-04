d1a5e6d705a07a0a5b485ec25e48ea9c1c2ef16d
Nobie <n12507400@gmail.com>
2023-08-04 15:07:55 -0700

    Addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add two numbers
+ * @argc: Arguement count
+ * @argv: Arguement vectors
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		j = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += j;
+	}
+	printf("%d\n", sum);
+
+	return (sum);
+}
