a503cfa8345526a30a005244d142232098b48543
Nobie <n12507400@gmail.com>
2023-08-04 13:26:55 -0700

    command line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ * @argc:Arguement Count
+ * @argv:Arguement Vectors
+ * Return:return 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
