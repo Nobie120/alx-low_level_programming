4e31ede52a8ace97bd4c856bfc2e0f71b8064317
Nobie <n12507400@gmail.com>
2023-08-11 14:52:18 -0700

    Multiplation

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - converts string to interger
+ * @s:the string
+ * Return:The interger
+ */
+
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int resp = 0, first, i;
+
+	for (first = 0; !(s[first] >= 48 && s[first] <= 57); first++)
+	{
+		if (s[first] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (i = first; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+ * print_int - prints interger
+ * @n:The int
+ * Return: 0
+ */
+
+void print_int(int n)
+{
+	unsigned long int divisor = 1, i, resp;
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+	;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+}
+/**
+ * main - multiply two integers
+ * @argc:arguement count
+ * @argv:arguement vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		puts("Error");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
