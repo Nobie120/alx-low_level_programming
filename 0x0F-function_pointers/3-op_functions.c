971e65f84c84004955daf08c04c3b2f542c1bd61
Nobie <n12507400@gmail.com>
2023-08-16 17:48:33 -0700

    Simple Calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add numbers
+ * @a:1st number
+ * @b:2nd number
+ * Return:The answer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract
+ * @a:fisrt num
+ * @b:second num
+ * Return:result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a:1st num
+ * @b:2nd num
+ * Return:result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ * @a:1st num
+ * @b:second num
+ * Return:result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modular
+ * @a:1st num
+ * @b:2nd num
+ * Return:result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
