57ea09d3bf7d0050163c236ecc68c72658874e3b
Nobie <n12507400@gmail.com>
2023-08-17 17:17:09 -0700

    Simple printf-like function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Function that prints anything
+ * @format:a list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i  = 0;
+	va_list value;
+	char *s, *sep = "";
+
+	va_start(value, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(value, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(value, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(value, double));
+					break;
+				case 's':
+					s = va_arg(value, char *);
+
+					if (!s)
+						printf("(nil)");
+					printf("%s%s", sep, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+		va_end(value);
+	}
+	printf("\n");
+}
