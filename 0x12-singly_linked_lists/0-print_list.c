612d752cfa9bfc22d0e621389728b4e54e9a9292
Nobie120 <n12507400@gmail.com>
2023-08-24 08:11:58 -0700

    displaying data in nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h:pointer to fisrt node
+ *
+ * Return:number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%ld] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
