31c1c2fa1ebf7d032411c13ee879a74b4f4e24fa
Nobie120 <n12507400@gmail.com>
2023-08-28 04:40:27 -0700

    Dispalyong the elements of nodes and counting all the nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint -  function that prints all the elements of a listint_t list
+ * @h:pointer to the 1st node
+ *
+ * Return:Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	const listint_t *temp;
+
+	temp = h;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
