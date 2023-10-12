c7d6edd93389d2f3ad9db0089ffecad7a900988b
Nobie120 <n12507400@gmail.com>
2023-10-12 15:11:10 -0700

    function that add node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,9 +26,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 		return (NULL);
+
 	newnode->n = n;
-	newnode->next = temp->next;
 	newnode->prev = temp;
+	newnode->next = temp->next;
 	temp->next->prev = newnode;
 	temp->next = newnode;
 
