d4a32fb05764234dc4439628b7ef14428a87be22
Nobie120 <n12507400@gmail.com>
2023-11-03 11:08:48 -0700

    a function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,37 @@
 #include "hash_tables.h"
 
 /**
+ * hash_node_create - creates a linked list with a node(s)
+ * @key:the key
+ * @value:value associated with the key
+ *
+ * Return: a pointer to node to the first node
+ */
+
+hash_node_t *hash_node_create(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (key == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	return (node);
+}
+
+/**
  * hash_table_set -  function that adds an element to the hash table.
  * @ht:pointer to the hash table
  * @key:the key
@@ -17,6 +48,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 		key == NULL || strlen(key) == 0 || value == NULL)
+	{
+		return (0);
+	}
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 	while (temp != NULL)
@@ -38,35 +72,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->next = ht->array[index];
 	ht->array[index] = node;
 	return (1);
-}
-
-/**
- * hash_node_create - creates a linked list with a node(s)
- * @key:the key
- * @value:value associated with the key
- *
- * Return: a pointer to node to the first node
- */
-
-hash_node_t *hash_node_create(const char *key, char *value)
-{
-	hash_node_t *node;
-
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (NULL);
-	node->key = strdup(key);
-	if (key == NULL)
-	{
-		free(node);
-		return (NULL);
-	}
-	node->value = strdup(value);
-	if (key == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (NULL);
-	}
-	return (node);
 }
